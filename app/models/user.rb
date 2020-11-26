64e48f862e3f3e922d5eda6a349651a35eedecd8
mikafill <kogorow.magic@gmail.com>
2020-11-26 22:56:50 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: {message: "必須項目です"} do
+    validates :nickname
+    validates :email, uniqueness: true, inclusion: {in: "@", message: "有効なメールアドレスを入力してください"}
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+    validates :encrypted_password, length: {minimum: 6, message: "6文字以上で入力してください"}, format: {with: PASSWORD_REGEX message: "半角英数字で入力してください"}, confirmation: true
+    validates :encrypted_password_confirmation {message: "パスワードが異なります"}
+    validates :birthday
+  end
+
+  with_options presence: {message: "必須項目です"}, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "全角で入力してください"}  do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: {message: "必須項目です"}, format: {with: /\A[ァ-ン]+\z/, message: "カタカナで入力してください"} do
+    validates :kana_first_name
+    validates :kana_last_name
+  end
 end
