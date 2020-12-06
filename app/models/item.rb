008665d9c8b4a3c4037f68c9fbbac90cfb704a52
mikafill <kogorow.magic@gmail.com>
2020-12-06 16:00:12 +0900

    各モデルでのアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,23 @@ class Item < ApplicationRecord
   with_options presece: { message: '必須項目です'} do
     validates :name
     validates :explanation
-    validates :categories_id
+    validates :category_id
     validates :condition_id
     validates :fee_type_id
     validates :delvery_area_id
-    validates :delivery_days_id
+    validates :delivery_day_id
     validates :price, numericality: { only_integer: true, message: '半角数字で入力してください' }
       , inclusion { in: 300..9999999, message: '300~9999999円の範囲で入力してください' }
   end
+
+  with_options numericality: {other_than: 1, message: '選択されていません'} do
+    validates :category_id
+    validates :condition_id
+    validates :fee_type_id
+    validates :delvery_area_id
+    validates :delivery_day_id
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id, :condition_id, :fee_type_id, :delvery_area_id, :delivery_day_id, :price
 end
--- db/migrate/20201206042430_create_items.rb
+++ db/migrate/20201206042430_create_items.rb
@@ -2,15 +2,15 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
 
-      t.string   :name,              null: false
-      t.text      :explanation,      null: false
-      t.integer   :categories_id,    null: false
-      t.integer   :condition_id,     null: false
-      t.integer   :fee_type_id,      null: false
-      t.integer   :delivery_area_id, null: false
-      t.integer   :deluvery_days_id, null: false
-      t.integer   :price,            null: false
-      t.reference :user,             null: false, foreign_key: true
+      t.string     :name,              null: false
+      t.text       :explanation,      null: false
+      t.integer    :category_id,      null: false
+      t.integer    :condition_id,     null: false
+      t.integer    :fee_type_id,      null: false
+      t.integer    :delivery_area_id, null: false
+      t.integer    :deluvery_day_id,  null: false
+      t.integer    :price,            null: false
+      t.references :user,             null: false, foreign_key: true
       t.timestamps
     end
   end
